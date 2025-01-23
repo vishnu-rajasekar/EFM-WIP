c7c011291f94c54e1bc7c8da4c5174846087559f
vishnu-rajasekar <74918366+vishnu-rajasekar@users.noreply.github.com>
2025-01-23 17:28:34 +0100

    Functional connector module & working directory

---
+++ src/WorkingFiles/c# script editor files/karamaba/C__Script-a3b56.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using Rhino;
+using Rhino.Geometry;
+
+using Grasshopper;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Data;
+using Grasshopper.Kernel.Types;
+
+using System.Threading;
+using System.Threading.Tasks;
+using System.Linq;
+using Karamba.Models;
+using KarambaCommon;
+
+
+/// <summary>
+/// This class will be instantiated on demand by the Script component.
+/// </summary>
+public abstract class Script_Instance_a3b56 : GH_ScriptInstance
+{
+  #region Utility functions
+  /// <summary>Print a String to the [Out] Parameter of the Script component.</summary>
+  /// <param name="text">String to print.</param>
+  private void Print(string text) { /* Implementation hidden. */ }
+  /// <summary>Print a formatted String to the [Out] Parameter of the Script component.</summary>
+  /// <param name="format">String format.</param>
+  /// <param name="args">Formatting parameters.</param>
+  private void Print(string format, params object[] args) { /* Implementation hidden. */ }
+  /// <summary>Print useful information about an object instance to the [Out] Parameter of the Script component. </summary>
+  /// <param name="obj">Object instance to parse.</param>
+  private void Reflect(object obj) { /* Implementation hidden. */ }
+  /// <summary>Print the signatures of all the overloads of a specific method to the [Out] Parameter of the Script component. </summary>
+  /// <param name="obj">Object instance to parse.</param>
+  private void Reflect(object obj, string method_name) { /* Implementation hidden. */ }
+  #endregion
+
+  #region Members
+  /// <summary>Gets the current Rhino document.</summary>
+  private readonly RhinoDoc RhinoDocument;
+  /// <summary>Gets the Grasshopper document that owns this script.</summary>
+  private readonly GH_Document GrasshopperDocument;
+  /// <summary>Gets the Grasshopper script component that owns this script.</summary>
+  private readonly IGH_Component Component;
+  /// <summary>
+  /// Gets the current iteration count. The first call to RunScript() is associated with Iteration==0.
+  /// Any subsequent call within the same solution will increment the Iteration count.
+  /// </summary>
+  private readonly int Iteration;
+  #endregion
+  /// <summary>
+  /// This procedure contains the user code. Input parameters are provided as regular arguments,
+  /// Output parameters as ref arguments. You don't have to assign output parameters,
+  /// they will have a default value.
+  /// </summary>
+  #region Runscript
+  private void RunScript(object Model_in, ref object A)
+  {
+    Toolkit k3d = new Toolkit();  
+    var model = Model_in as Model;
+    var elem = model.elems[0];
+    A = elem;
+  }
+  #endregion
+  #region Additional
+
+  #endregion
+}
